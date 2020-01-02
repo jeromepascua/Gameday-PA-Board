91f8227ea007c661efecdc8034805b1dbdb05275
Jerome Pascua <jerome.pascua@natus.com>
2020-01-02 18:18:43 -0500

    Added a cheap, free theme

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const applicationName = 'Gameday Announcer Board';
+app.name = applicationName;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
